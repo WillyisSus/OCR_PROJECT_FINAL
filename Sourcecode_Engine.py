6c2e8a44d186bef9926e69e8a75b96bd0a7757aa
WillyisSus <vovietlong0845927889@gmail.com>
2022-12-11 21:36:48 +0700

    Update Sourcecode_Engine.py

--- Sourcecode_Engine.py
+++ Sourcecode_Engine.py
@@ -5,17 +5,16 @@ from PIL import Image, ImageTk
 
 root = Tk()
 root.title('My Image to Text')
-
+root.geometry("900x600")
 def file_open():
     global my_image, image_path
     root.filename = filedialog.askopenfilename(initialdir="D:\DO_AN_THUC_HANH\OCR_CASE_TEST", title= "Select a file", filetypes =(("png files", "*.png"),("*.jpg", "jpg files"),("all files"))) 
-    my_label = Label(root, text = root.filename).pack()
     image_path = root.filename
     my_image = ImageTk.PhotoImage(Image.open(root.filename))
     my_image_label = Label(image=my_image).pack()
 
-my_btn = Button(root, text = "Open File", command = file_open).pack()
-
+my_button = Button(root, text = "Open File", command = file_open).pack()
+my_button = Button(root, text = "Start Process", command = root.destroy).pack()
 root.mainloop()
 
 # Paddleocr supports Chinese, English, French, German, Korean and Japanese.
@@ -29,10 +28,7 @@ for idx in range(len(result)):
         print(line)
 
 # draw result
-import fitz
 from PIL import Image
-import cv2
-import numpy as np
 imgs = []
 for idx in range(len(result)):
     res = result[idx]
